d9675e9894c30a51e6391b4112693cb8ff6847c4
Akshay Pandey <akshaypandey.8996@gmail.com>
2025-10-21 13:58:47 +0530

    Initial Spring Boot setup

---
+++ coursemanagementportal/src/test/java/com/AksHashes/coursemanagementportal/CourseManagementPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AksHashes.coursemanagementportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
